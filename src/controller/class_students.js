e3f36e89c27f3bfd0a99ca4ef22f7e040190b69c
Kat Reinhart <kat.reinhart@gmail.com>
2017-11-10 13:38:48 -0800

    Remove rogue console.log

--- src/controller/class_students.js
+++ src/controller/class_students.js
@@ -1,9 +1,7 @@
 const { classStudentModel: model } = require('../model')
 
 const getClassStudents = (req, res, next) => {
-  console.log('get class students')
   model.getClassStudents(req.params.id).then(roster => {
-    console.log(roster)
     res.status(200).json({ roster })
   })
 }
