0cb4e8d3ed936d502069f6488715b30883462e1b
Kat Reinhart <kat.reinhart@gmail.com>
2017-11-16 09:37:32 -0800

    Dbug DB issues

--- src/db/seeds/02_units.js
+++ src/db/seeds/02_units.js
@@ -1,9 +1,9 @@
 exports.seed = knex => {
   return knex('units').del().then(() => {
     return knex('units').insert([
-      { id: 1, course_id: 1, title: 'Rocks and Minerals', summary: 'Freegan farm-to-table gastropub tousled, typewriter vegan hammock ennui ethical blue bottle. Occupy kickstarter wolf health goth snackwave pour-over.' },
-      { id: 2, course_id: 1, title: 'Anatomy of the Earth', summary: 'Freegan farm-to-table gastropub tousled, typewriter vegan hammock ennui ethical blue bottle. Occupy kickstarter wolf health goth snackwave pour-over.' },
-      { id: 3, course_id: 2, title: 'The Skeleton', summary: 'Freegan farm-to-table gastropub tousled, typewriter vegan hammock ennui ethical blue bottle. Occupy kickstarter wolf health goth snackwave pour-over.' }
+      { id: 1, course_id: 1, title: 'Rocks and Minerals', summary_text: 'Freegan farm-to-table gastropub tousled, typewriter vegan hammock ennui ethical blue bottle. Occupy kickstarter wolf health goth snackwave pour-over.' },
+      { id: 2, course_id: 1, title: 'Anatomy of the Earth', summary_text: 'Freegan farm-to-table gastropub tousled, typewriter vegan hammock ennui ethical blue bottle. Occupy kickstarter wolf health goth snackwave pour-over.' },
+      { id: 3, course_id: 2, title: 'The Skeleton', summary_text: 'Freegan farm-to-table gastropub tousled, typewriter vegan hammock ennui ethical blue bottle. Occupy kickstarter wolf health goth snackwave pour-over.' }
     ])
   })
   .then(() => {
